e96ba5b1c7aab8dfccc7dcf592829e8f89151913
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2021-08-23 13:48:28 +0530

    modified:   .gitignore 	new file:   MountainCar/FixedInitStateExpt.py 	new file:   MountainCar/GranularStatesExpt.py 	modified:   MountainCar/expt.py Derived child expt classes using OOP.

---
+++ MountainCar/GranularStatesExpt.py
@@ -0,0 +1,17 @@
+from expt import BaseExperiment
+
+
+class GranularStatesExpt(BaseExperiment):
+
+  # Settings for discreti-sing the observed continuous state values
+  DISCRETE_BUCKET_SIZES = [40, 40]
+
+  # Settings for Q-Learning
+  EPISODES = 10_000
+
+
+
+if __name__=='__main__':
+
+  expt = GranularStatesExpt()
+  expt.run()
--- MountainCar/expt.py
+++ MountainCar/expt.py
@@ -78,14 +78,9 @@ class BaseExperiment(object):
 
 
   def init_episode_state(self):
-    # A. Default Init : places the agent in the env
-    #      at a random position and having random velocity
+    # Default Init : places the agent in the env
+    #   at a random position and having random velocity
     discrete_state = self.get_discrete_state(self.env.reset())
-
-    # A+B. Override the default init to always start the agent in a fixed state
-    discrete_state = self.get_discrete_state(np.array([-0.4, 0]))
-    self.env.env.state = np.array([-0.4, 0])
-
     return discrete_state
 
 
