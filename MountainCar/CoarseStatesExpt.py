bb57e216dfa55401b463fcec7ee145d41f73b52c
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2021-08-23 14:44:22 +0530

    new file:   MountainCar/CoarseStatesExpt.py

---
+++ MountainCar/CoarseStatesExpt.py
@@ -0,0 +1,17 @@
+from expt import BaseExperiment
+
+
+class CoarseStatesExpt(BaseExperiment):
+
+  # Settings for discreti-sing the observed continuous state values
+  DISCRETE_BUCKET_SIZES = [10, 10]
+
+  # Settings for Q-Learning
+  EPISODES = 10_000
+
+
+
+if __name__=='__main__':
+
+  expt = CoarseStatesExpt()
+  expt.run()
